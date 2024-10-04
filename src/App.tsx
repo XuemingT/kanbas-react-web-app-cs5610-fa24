2f9fb26783c82525f15859725884ab4c6db13bdd
XuemingT <xmtang810@gmail.com>
2024-10-04 16:46:12 -0700

    a2 landing pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,14 @@ import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
     <HashRouter>
-      {" "}
       <div>
-        {" "}
         <Routes>
           {" "}
-          <Route path="/" element={<Navigate to="Kanbas" />} />{" "}
-          <Route path="/Labs/*" element={<Labs />} />{" "}
+          <Route path="/" element={<Navigate to="Kanbas" />} />
+          <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
-        </Routes>{" "}
-      </div>{" "}
+        </Routes>
+      </div>
     </HashRouter>
   );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,9 +7,27 @@ export default function Labs() {
   return (
     <div>
       <h1> Labs </h1>
+      <h1> Xueming Tang</h1>
+      <h2>Section1</h2>
+      <h2>
+        <a
+          href="https://github.com/XuemingT/kanbas-react-web-app-cs5610-fa24/tree/a1"
+          target="blank"
+        >
+          code for a1
+        </a>
+      </h2>
+      <h2>
+        <a
+          href="https://github.com/XuemingT/kanbas-react-web-app-cs5610-fa24/tree/a2"
+          target="blank"
+        >
+          code for a2
+        </a>
+      </h2>
       <TOC />
       <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />{" "}
+        {/* <Route path="/" element={<Navigate to="Lab1" />} />{" "} */}
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
