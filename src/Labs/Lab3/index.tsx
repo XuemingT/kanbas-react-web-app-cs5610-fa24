4d8d9e3e0126b79fd88ab173e77a543624ece06f
XuemingT <xmtang810@gmail.com>
2024-10-19 20:02:21 -0700

    a3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,9 +1,53 @@
+import VariableTypes from "./VariableTypes";
 import VariablesAndConstants from "./VariablesAndConstants";
+import BooleanVariables from "./BooleanVariables";
+import TernaryOperator from "./TernaryOperator";
+import Ifelse from "./IfElse";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
 export default function Lab3() {
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
       <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <Ifelse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoList />
+      <Spreading />
     </div>
   );
 }
