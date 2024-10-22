1ed2d879735cda4e3dd1e0449179cc06a2c07469
XuemingT <xmtang810@gmail.com>
2024-10-22 01:09:14 -0700

    a3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -21,7 +21,15 @@ import JsonStringify from "./JsonStringify";
 import House from "./House";
 import TodoList from "./todos/TodoList";
 import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import Math from "./Math";
+import DestructingImports from "./DestructingImports";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
 export default function Lab3() {
+  console.log("Hello World!");
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
@@ -48,6 +56,21 @@ export default function Lab3() {
       <House />
       <TodoList />
       <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <h3>JavaScript</h3>
+      <Square>4</Square>
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione
+        eaque illo minus cum, saepe totam vel nihil repellat nemo explicabo
+        excepturi consectetur. Modi omnis minus sequi maiores, provident
+        voluptates.
+      </Highlight>
+
+      <hr />
     </div>
   );
 }
