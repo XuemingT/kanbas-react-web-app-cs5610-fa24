a8ed3c47a051de060977bc1d9839999d16c8075e
XuemingT <xmtang810@gmail.com>
2024-11-09 12:16:03 -0800

    a4

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,52 +1,53 @@
-import React, { useState } from "react";
-import ModuleControlButtons from "./ModuleControlButtons";
+import { useSelector, useDispatch } from "react-redux";
+import { addModule, editModule, updateModule, deleteModule } from "./reducer";
+import { useState } from "react";
+import LessonControlButtons from "./LessonControlButtons";
 import ModulesControls from "./ModulesControls";
 import { BsGripVertical } from "react-icons/bs";
 import { useParams } from "react-router-dom";
-import * as db from "../../Database";
-import LessonControlButtons from "./LessonControlButtons";
-import { addModule, editModule, updateModule, deleteModule } from "./reducer";
-import { useSelector, useDispatch } from "react-redux";
-
-// Add interface for better type safety
-interface Module {
-  _id: string;
-  name: string;
-  course: string;
-  editing?: boolean;
-  lessons: any[]; // Replace with proper Lesson interface if available
-}
+import ModuleControlButtons from "./ModuleControlButtons";
 
 export default function Modules() {
   const { cid } = useParams();
   const [moduleName, setModuleName] = useState("");
-  const { modules } = useSelector((state: any) => state.modulesReducer);
+
+  // Ensure modules is retrieved as an array from Redux
+  const modules = useSelector(
+    (state: any) => state.modulesReducer.modules || []
+  );
+
   const dispatch = useDispatch();
+
+  // Function to add a new module
+  const addNewModule = () => {
+    dispatch(addModule({ name: moduleName, course: cid })); // Dispatch Redux action
+    setModuleName(""); // Clear the input
+  };
 
   return (
     <div className="wd-modules">
       <ModulesControls
         moduleName={moduleName}
         setModuleName={setModuleName}
-        addModule={() => {
-          dispatch(addModule({ name: moduleName, course: cid }));
-          setModuleName("");
-        }}
+        addModule={addNewModule}
       />
-
-      <ul id="wd-modules" className="list-group rounded-0">
+      <br />
+      <br />
+      <ul className="mt-2 list-group rounded-0">
         {modules
-          .filter((module: Module) => module.course === cid)
-          .map((module: Module) => (
-            <li key={module._id} className="list-group-item">
-              <div className="d-flex align-items-center">
+          .filter((module: any) => module.course === cid)
+          .map((module: any) => (
+            <li
+              key={module._id}
+              className="wd-module list-group-item p-0 mb-5 fs-5 border-gray"
+            >
+              <div className="wd-title p-3 ps-2 bg-secondary">
                 <BsGripVertical className="me-2 fs-3" />
-
-                {!module.editing ? (
-                  <span>{module.name}</span>
-                ) : (
+                {!module.editing && module.name}
+                {module.editing && (
                   <input
                     className="form-control w-50 d-inline-block"
+                    defaultValue={module.name}
                     onChange={(e) =>
                       dispatch(
                         updateModule({ ...module, name: e.target.value })
@@ -57,19 +58,14 @@ export default function Modules() {
                         dispatch(updateModule({ ...module, editing: false }));
                       }
                     }}
-                    defaultValue={module.name}
                   />
                 )}
-
                 <ModuleControlButtons
                   moduleId={module._id}
-                  deleteModule={(moduleId) => {
-                    dispatch(deleteModule(moduleId));
-                  }}
+                  deleteModule={(moduleId) => dispatch(deleteModule(moduleId))}
                   editModule={(moduleId) => dispatch(editModule(moduleId))}
                 />
               </div>
-
               <ul className="wd-lessons list-group rounded-0">
                 {module.lessons &&
                   module.lessons.map((lesson: any) => (
