f6d1f81c75102aef0c6478c7bb8a02e95f26b27b
XuemingT <xmtang810@gmail.com>
2024-11-13 15:57:16 -0800

    a4

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,14 +1,33 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 export default function AccountNavigation() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+
   return (
     <div id="wd-account-navigation">
       <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">
         Northeastern
       </a>
       <br />
-      <Link to={`/Kanbas/Account/Signin`}> Signin </Link> <br />{" "}
-      <Link to={`/Kanbas/Account/Signup`}> Signup </Link> <br />{" "}
-      <Link to={`/Kanbas/Account/Profile`}> Profile </Link> <br />{" "}
+
+      {/* Show Signin and Signup only if user is NOT logged in */}
+      {!currentUser && (
+        <>
+          <Link to="/Kanbas/Account/Signin">Signin</Link>
+          <br />
+          <Link to="/Kanbas/Account/Signup">Signup</Link>
+          <br />
+        </>
+      )}
+
+      {/* Show Profile only if user is logged in */}
+      {currentUser && (
+        <>
+          <Link to="/Kanbas/Account/Profile">Profile</Link>
+          <br />
+        </>
+      )}
     </div>
   );
 }
