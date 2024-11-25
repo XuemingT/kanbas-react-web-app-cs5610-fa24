06ee78fb65b47de17a71e5df3c28c04cb306ea93
XuemingT <xmtang810@gmail.com>
2024-11-24 18:38:46 -0800

    lab 5

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -5,11 +5,12 @@ import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
 import store from "./store";
+import Lab5 from "./Lab5";
 import { Provider } from "react-redux";
 export default function Labs() {
   return (
     <Provider store={store}>
-      <div>
+      <div className="container-fluid">
         <h1> Labs </h1>
         <h1> Xueming Tang</h1>
         <h2>Section1</h2>
@@ -52,6 +53,7 @@ export default function Labs() {
           <Route path="Lab2" element={<Lab2 />} />
           <Route path="Lab3/*" element={<Lab3 />} />
           <Route path="Lab4/*" element={<Lab4 />} />
+          <Route path="Lab5/*" element={<Lab5 />} />
         </Routes>{" "}
       </div>
     </Provider>
